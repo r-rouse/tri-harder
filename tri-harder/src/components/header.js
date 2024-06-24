b7fb936aa90561eb575c8beb4fda0be0781317a1
Randall Rouse <randall.g.rouse@gmail.com>
2024-06-24 16:14:24 -0700

    feat: autoplay header video for phone

--- tri-harder/src/components/header.js
+++ tri-harder/src/components/header.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../styles/header.css"
 import NavBar from "./navBar";
 
 const Header = () => {
 
+    const videoRef = useRef(null);
 
+    useEffect(() => {
+      const video = videoRef.current;
+      if (video) {
+        const playPromise = video.play();
+        if (playPromise !== undefined) {
+          playPromise.then(() => {
+            // Autoplay started
+          }).catch(error => {
+            // Autoplay failed
+            console.log('Autoplay failed:', error);
+          });
+        }
+      }
+    }, []);
     return (
         <div className="component-container">
             <div className="header-container">
                 <div className="header-logo">
                     <h1 className="logo">TRI-HARDER</h1>
-                  <video autoPlay loop muted playsinline class="elementor-background-video-hosted elementor-html5-video"  src="https://runlongbeach.com/wp-content/uploads/2022/10/LBM-Horizontal-for-Web.mp4" ></video>
+                    <video
+                        ref={videoRef}
+                        autoPlay
+                        loop
+                        muted
+                        playsInline
+                        className="elementor-background-video-hosted elementor-html5-video"
+                        src="https://runlongbeach.com/wp-content/uploads/2022/10/LBM-Horizontal-for-Web.mp4"
+                    ></video>
+
                 </div>
             </div>
             <div className="logo-container">
-                <NavBar/>
+                <NavBar />
             </div>
         </div>
     )
