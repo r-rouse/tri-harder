3b40463c07da3dc69cbd03f3221079e8f4ade8df
Randall Rouse <randall.g.rouse@gmail.com>
2024-06-25 11:37:36 -0700

    fix: course maps

--- tri-harder/src/pages/course.js
+++ tri-harder/src/pages/course.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import NavBar from '../components/navBar';
+import React from "react";
+import image from "../assets/course1.png";
+import image2 from "../assets/course2.png";
+import image3 from "../assets/course3.png";
+import image4 from "../assets/course4.png";
+import image5 from "../assets/course5.png";
+import NavBar from "../components/navBar";
 import '../styles/course.css';
-import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-
-// Configure the worker
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Course = () => {
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
-
     return (
         <>
             <div className="logo-container">
                 <NavBar />
             </div>
-            <div className="pdf-container">
-                <Worker workerUrl={`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`}>
-                    <Viewer
-                        fileUrl={`${process.env.PUBLIC_URL}/tri-harder.pdf`}
-                        plugins={[defaultLayoutPluginInstance]}
-                    />
-                </Worker>
+            <div className="full-screen-image">
+                <img src={image} alt="course map" />
+                <img src={image2} alt="course map" />
+                <img src={image3} alt="course map" />
+                <img src={image4} alt="course map" />
+                <img src={image5} alt="course map" />
             </div>
         </>
     );
-};
+}
 
 export default Course;
-
